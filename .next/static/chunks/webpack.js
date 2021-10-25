52edcf9eb291f3e191141e09ffc431db6a250c23
Ari Cheriyan <mcheriyan@college.harvard.edu>
2021-10-25 17:23:53 -0400

    Updated .gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9109e48efb59742394fe"; }
+/******/ 		__webpack_require__.h = function() { return "9bb090d813192e7bd64b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.5b322e8dce7668a1bd53.hot-update.js
+++ .next/static/webpack/webpack.5b322e8dce7668a1bd53.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c64525f2485b1f92985b"; }
+/******/ 	__webpack_require__.h = function() { return "5b7b16f594529b6d1fd4"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
