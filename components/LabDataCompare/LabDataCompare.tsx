fe92b950693cbc7cc0ee4686c4269f740031c425
Ari Cheriyan <mcheriyan@college.harvard.edu>
2021-10-27 22:25:44 -0400

    Update from meeting w Rakesh

--- components/LabDataCompare/LabDataCompare.tsx
+++ components/LabDataCompare/LabDataCompare.tsx
@@ -8,33 +8,81 @@ import "react-datepicker/dist/react-datepicker.css";
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import { useQuery, useMutation } from "urql";
 import {XYPlot, XAxis, YAxis, HorizontalGridLines, ChartLabel, VerticalGridLines, LineMarkSeries, LineSeries, DiscreteColorLegend} from 'react-vis';
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  Select,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 
+export type LabCategory = {
+  lab: string;
+  temperature: number;
+};
 
 const LabDataCompare: React.FC = (props) => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+  
   const handleClose = () => {
     setAnchorEl(null);
   };
-  const [otherinfo, setOtherInfo] = useState<number | null>(0);
+  var [otherinfo, setOtherInfo] = useState<number>(0);
 
   {/* Load labs from Hasura, for now its hard coded*/}
   const labsData = ["B-15", "302", "303", "446"];
-  const b15_temp = [{x: 10, y: 11}, {x: 150, y: 29}, {x: 300, y: 7}, {x: 400, y: 37}, {x: 500, y: 47},{x: 1000, y: 71} ]
-  const room302_temp = [{x: 8, y: 11}, {x: 130, y: 20}, {x: 250, y: 18}, {x: 450, y: 50}, {x: 490, y: 47},{x: 675, y: 80} ]
+  var b15_temp = [{x: 10, y: 11}, {x: 150, y: null}, {x: 300, y: null}, {x: 400, y: 37}, {x: 500, y: 47},{x: 1000, y: 71} ]
+  const room302_temp = [{x: 8, y: null}, {x: 130, y: 20}, {x: 250, y: null}, {x: 450, y: 50}, {x: 490, y: 47},{x: 675, y: 80} ]
   const room302_hum = [{x: 18, y: 11}, {x: 110, y: 20}, {x: 210, y: 18}, {x: 410, y: 50}, {x: 510, y: 47},{x: 610, y: 80} ]
-  const b15_hum = [{x: 38, y: 11}, {x: 160, y: 20}, {x: 280, y: 18}, {x: 490, y: 50}, {x: 560, y: 47},{x: 620, y: 80} ]
+  const b15_hum = [{x: 38, y: 11}, {x: 160, y:null}, {x: 280, y: null}, {x: 490, y: 50}, {x: 560, y: 47},{x: 620, y: 80} ]
   const labtempIndex = [b15_temp, room302_temp, b15_temp, room302_temp]
   const labhumIndex = [b15_hum, room302_hum, b15_hum, room302_hum]
   const labLabels = ["B-15 Humidity", 'B-15 Temperature', "302 Humidity", "302 Temperature"]
   
+  const parameters = ["Humidity","Temperature",];
+  const [param, setParam] = useState<number | null>(0);
+
   {/* Set up date picker*/}
   const [dateRange, setDateRange] = useState([null, null]);
   const [startDate, endDate] = dateRange;
+
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    otherinfo = 1;
+    setAnchorEl(event.currentTarget);
+    
+  };
+
+  function handleClick2(num) {
+    console.log(num);
+  };
+
+  const labQuery = `
+  query MyQuery {
+    observations {
+      lab
+      temperature
+    }
+  }
+  
+  `;
+
+
+  const [{ data, fetching, error }, executeQuery] = useQuery({
+    query: labQuery
+  });
+
+  if (data != null){
+    console.log(data.observations[0].lab);
+  } 
+  else{
+    console.log("failed")
+  }
+  
 
   return (
     <>
@@ -47,13 +95,14 @@ const LabDataCompare: React.FC = (props) => {
         }}
         
       >
+
         <Button
             id="basic-button"
             aria-controls="basic-menu"
             variant= "contained"
             aria-haspopup="true"
             aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
+            onClick={() => handleClick2(1)}
           >
             Lab Select 1
         </Button>
@@ -63,7 +112,9 @@ const LabDataCompare: React.FC = (props) => {
             variant= "contained"
             aria-haspopup="true"
             aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
+            onClick= {() => 
+              handleClick2(2)
+            }
           >
             Lab Select 2
         </Button>
@@ -77,20 +128,24 @@ const LabDataCompare: React.FC = (props) => {
               'aria-labelledby': 'basic-button',
             }}
 
-            value = {otherinfo}
-              onChange={(event: React.ChangeEvent<{ value: unknown }>) => {
-                setOtherInfo(event.target.value as number);
-              }}
             > 
-        
-            {/* Populate menu items with labs*/}
-            {labsData.map((lab, index) => {
+
+              {labsData.map((lab) => {
                       return (
-                        <MenuItem key={index} value={index} onClick={handleClose}>{lab}</MenuItem>
+                        <MenuItem key={lab} onClick={handleClose}>{lab}</MenuItem>
                 )})} 
+
+            {/* Populate menu items with labs
+            {data ?? data.observations.map(((DataS: LabCategory) => {
+                  return (
+                    <MenuItem key={DataS.lab} value={DataS.temperature} onClick={handleClose}>{DataS.lab}</MenuItem>
+               )}))
+              } 
+              */}
         </Menu>
         
         
+
         <DatePicker
       selectsRange={true}
       startDate={startDate}
@@ -111,7 +166,7 @@ const LabDataCompare: React.FC = (props) => {
           <DiscreteColorLegend items={labLabels} orientation={"horizontal"}/>
           <LineMarkSeries 
             curve={'curveMonotoneX'}
-            data={labtempIndex[0]}
+            data={labtempIndex[otherinfo]}
             opacity = {1}
             style= {{fill: 'none'}}
           />
