3733c4e332ec5b1e8242b93943681b944f857997
Maria Cheriyan <57424289+mcheriyan@users.noreply.github.com>
2021-11-30 20:28:22 -0500

    Update home-page.tsx

--- pages/home-page.tsx
+++ pages/home-page.tsx
@@ -25,7 +25,7 @@ const HomePage: React.FC = (props) => {
   return (
     <>
     <Container>
-           {/* <LabDataView/>  */}
+           <LabDataView/>
 
       <Box m = {5}> 
         Homepage
