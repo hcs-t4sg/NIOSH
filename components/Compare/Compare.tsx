7c2cd9837f653aac8c1b1a202b623fb9abcaf2a8
Aneesh Muppidi <aneeshmuppidi19@gmail.com>
2021-12-01 21:44:50 -0500

    Compare page with graph reloading

--- components/Compare/Compare.tsx
+++ components/Compare/Compare.tsx
@@ -9,7 +9,7 @@ import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { useQuery, useMutation } from "urql";
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, ChartLabel, VerticalGridLines, LineMarkSeries, LineSeries, DiscreteColorLegend, Highlight} from 'react-vis';
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, ChartLabel, VerticalGridLines, LineMarkSeries, LineSeries, DiscreteColorLegend} from 'react-vis';
 import {
   Box,
   FormControl,
@@ -27,114 +27,152 @@ export type LabCategory = {
 const LabDataCompare: React.FC = (props) => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+  const labsData = [];
   
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  var [otherinfo, setOtherInfo] = useState<number>(0);
-
-  {/* Load labs from Hasura, for now its hard coded*/}
-  const labsData = ["B-15", "302", "303", "446"];
-  var b15_temp = [{x: 10, y: 11}, {x: 150, y: null}, {x: 300, y: null}, {x: 400, y: 37}, {x: 500, y: 47},{x: 1000, y: 71} ]
-  const room302_temp = [{x: 8, y: null}, {x: 130, y: 20}, {x: 250, y: null}, {x: 450, y: 50}, {x: 490, y: 47},{x: 675, y: 80} ]
-  const room302_hum = [{x: 18, y: 11}, {x: 110, y: 20}, {x: 210, y: 18}, {x: 410, y: 50}, {x: 510, y: 47},{x: 610, y: 80} ]
-  const b15_hum = [{x: 38, y: 11}, {x: 160, y:null}, {x: 280, y: null}, {x: 490, y: 50}, {x: 560, y: 47},{x: 620, y: 80} ]
-  const labtempIndex = [b15_temp, room302_temp, b15_temp, room302_temp]
-  const labhumIndex = [b15_hum, room302_hum, b15_hum, room302_hum]
-  const labLabels = ["T454 Hum", 'T454 Temp', 'H309 Hum', 'H309 Temp']
-  const colorLabels = ["#352D39", "#352D39", "#352D39", "#352D39"]
-  const labels = [{title: 'T454 Hum', color: '#9c88ff'}, {title: 'T454 Temp', color: '#273c75'}, {title: 'H309 Hum', color: '#b71540'}, {title: 'H309 Temp', color: '#e55039'}]
-  const t454_hum = []
-  const t454_temp = []
-  const h309_hum = []
-  const h309_temp = []
+  const labels = [{title: 'T454 Hum', color: '#9c88ff'}, {title: 'T454 Temp', color: '#273c75'}]
+  const t454_hum = [{x: 18, y: 11}, {x: 110, y: 20}]
+  const t454_temp = [{x: 38, y: 11}, {x: 160, y:150}]
+  
   const parameters = ["Humidity","Temperature",];
   const [param, setParam] = useState<number | null>(0);
 
   {/* Set up date picker*/}
   const [dateRange, setDateRange] = useState([null, null]);
   const [startDate, endDate] = dateRange;
-
+  var [lab_hum, set_labhum] = useState([])
+  var [lab_temp, set_labtemp] = useState([])
+  var [lab_hum2, set_labhum2] = useState([])
+  var [lab_temp2, set_labtemp2] = useState([])
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    otherinfo = 1;
     setAnchorEl(event.currentTarget);
-    
   };
 
-  function handleClick2(num: number) {
-    console.log(num);
+  const handleClick2 = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  const labQuery1 = `
-  query MyQuery {
-    observations(where: {time: {_gte: "2021-05-18", _lt: "2021-05-19"}, humidity: {_is_null: false}, lab: {_eq: "T454"}}) {
+  const labQueryName = `
+  query queryLab {
+    observations() {
       lab
-      temperature
       humidity
-      time
-    }
-  }  
-  
-  `;
-
-  const labQuery2 = `
-  query MyQuery {
-    observations(where: {_or: [{time: {_gte: "2021-05-18", _lt: "2021-05-22"}, humidity: {_is_null: false}, lab: {_eq: "T454"}}, {time: {_gte: "2021-05-18", _lt: "2021-05-22"}, humidity: {_is_null: false}, lab: {_eq: "H309"}}]}) {
       temperature
       time
-      humidity
-      lab
     }
-  }  
+  }
+  `; 
   
-  `;
 
-
-  var [{ data, fetching, error }, executeQuery] = useQuery({
-    query: labQuery2
+  const [{ data, fetching, error }, executeQuery] = useQuery({
+    query: labQueryName
   });
 
 
-  if (fetching){
-    console.log("Fetching")
-  }
-  else {
-    let someArray = [1, "string", false];
-    for (let entry of data.observations) {
-      var hour = Number(entry.time.split("T")[1].substring(0,2));
-      var timestamp = new Date(entry.time).getTime();
-      const timestamp2 = new Date('September 10 2017').getTime();
-      console.log(timestamp)
-      console.log(timestamp2)
-      console.log(entry.time.split("T")[0]+" "+entry.time.split("T")[1])
-      var dt = entry.time.split("T")[0]+" "+entry.time.split("T")[1]
-      if (entry.lab == "H309")
-      {
-        h309_hum.push({x: timestamp, y: entry.humidity});
-        h309_temp.push({x: timestamp, y: entry.temperature});
-      }
-      if(entry.lab == "T454")
-      {
-        t454_hum.push({x: timestamp, y: entry.humidity});
-        t454_temp.push({x: timestamp, y: entry.temperature});
-      }
-      
-      }
-  
-  }
-  
   if (data != null){
-    console.log(data.observations);
+    console.log("data is not null");
+    
   } 
   else{
     console.log("failed")
   }
+  let someArray = [1, "string", false];
 
-console.log(t454_hum);
-console.log(h309_hum);
-console.log("Start Date")
-console.log(startDate)
+  
+  if (fetching){
+    console.log("Fetching")
+  }
+  else {
+    for (let entry of data.observations) {  
+      if (labsData.includes(entry.lab)) {
+      console.log("Done")
+      }
+      else{
+        labsData.push(entry.lab) 
+        } 
+     
+    };
+  
+  }
+
+
+  
+  const handleClose = () => {
+    setAnchorEl(null);
+    const index = Math.floor((Math.random())*4)
+    var hour = 0
+    
+    console.log("HUMIDITY IS:" + t454_hum)
+    console.log("Start date get time is " + startDate + new Date(startDate).getTime())
+ 
+    
+
+  };
+
+  const handleMenuItem = event => {
+    const { myLab } = event.currentTarget.dataset
+    console.log(myLab)
+    lab_hum = []
+    lab_temp = []
+    var hour = 0
+    for (let entry of data.observations) {  
+      if (entry.lab == myLab) {
+        var hour = new Date(entry.time).getTime();
+        if (hour > new Date(startDate).getTime()) {
+          if (hour < new Date(endDate).getTime()) {
+          console.log("this is also working")
+          lab_hum = lab_hum.concat({x: hour, y: entry.humidity})
+          set_labhum(lab_hum);
+          lab_temp = lab_temp.concat({x: hour, y: entry.temperature}) 
+          set_labtemp(lab_temp);
+          t454_hum.push({x: hour, y: entry.humidity})
+      }}}
+      else{
+        console.log("Data done")
+      }
+    };
+
+
+    console.log(lab_hum)
+    console.log(lab_temp)
+    console.log(t454_hum)
+    console.log("Start date get time is " + startDate + new Date(startDate).getTime())
+ 
+    
+
+  }
+  const handleMenuItem2 = event => {
+    const { myLab } = event.currentTarget.dataset
+    console.log(myLab)
+    lab_hum2 = []
+    lab_temp2 = []
+    var hour = 0
+    for (let entry of data.observations) {  
+      if (entry.lab == myLab) {
+        var hour = new Date(entry.time).getTime();
+        if (hour > new Date(startDate).getTime()) {
+          if (hour < new Date(endDate).getTime()) {
+          console.log("this is also working")
+          lab_hum2 = lab_hum2.concat({x: hour, y: entry.humidity})
+          set_labhum2(lab_hum2);
+          lab_temp2 = lab_temp2.concat({x: hour, y: entry.temperature}) 
+          set_labtemp2(lab_temp2);
+      }}}
+      else{
+        console.log("Data done")
+      }
+    };
+
+
+    console.log(lab_hum)
+    console.log(lab_temp)
+    console.log(t454_hum)
+    console.log("Start date get time is " + startDate + new Date(startDate).getTime())
+ 
+    
+
+  }
+
   return (
     <>
       <Container
@@ -149,29 +187,16 @@ console.log(startDate)
         
       >
 
-        <Button
+    <Button
             id="basic-button"
             aria-controls="basic-menu"
             variant= "contained"
             aria-haspopup="true"
             aria-expanded={open ? 'true' : undefined}
-            onClick={() => handleClick2(1)}
+            onClick={handleClick}
           >
-            Lab Select 1
+            Lab 1
         </Button>
-        <Button
-            id="basic-button"
-            aria-controls="basic-menu"
-            variant= "contained"
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick= {() => 
-              handleClick2(2)
-            }
-          >
-            Lab Select 2
-        </Button>
-        {/* Lab selection buttons*/}
         <Menu
             id="basic-menu"
             anchorEl={anchorEl}
@@ -180,25 +205,38 @@ console.log(startDate)
             MenuListProps={{
               'aria-labelledby': 'basic-button',
             }}
-
-            > 
-
-              {labsData.map((lab) => {
+        >
+            {/* Populate menu items with labs*/}
+            {labsData.map((lab) => {
                       return (
-                        <MenuItem key={lab} onClick={handleClose}>{lab}</MenuItem>
+                        <MenuItem key={lab} data-my-lab={lab} onClick={handleMenuItem}>{lab}</MenuItem>
                 )})} 
-
-            {/* Populate menu items with labs
-            {data ?? data.observations.map(((DataS: LabCategory) => {
-                  return (
-                    <MenuItem key={DataS.lab} value={DataS.temperature} onClick={handleClose}>{DataS.lab}</MenuItem>
-               )}))
-              } 
-              */}
         </Menu>
-        
-        
-
+        <Button
+            id="basic-button"
+            aria-controls="basic-menu"
+            variant= "contained"
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+          >
+            Lab 2
+        </Button>
+        <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+        >
+            {/* Populate menu items with labs*/}
+            {labsData.map((lab) => {
+                      return (
+                        <MenuItem key={lab} data-my-lab={lab} onClick={handleMenuItem2}>{lab}</MenuItem>
+                )})} 
+        </Menu>
         <DatePicker
       selectsRange={true}
       startDate={startDate}
@@ -210,45 +248,49 @@ console.log(startDate)
     /> 
 
         {/* Load lab data on graph, for now, hard-coded*/}
-        <XYPlot width={1000} height={500}>
-          <DiscreteColorLegend items={labels} orientation={"horizontal"}/>
+        <XYPlot width={1000} height={800} margin={{left: 100, right: 10, top: 10, bottom:300}}>
           <VerticalGridLines />
           <HorizontalGridLines />
-          <XAxis title = "Hour"/>
           <YAxis title = "Temperature/Humidity (°F)"/>
+          <XAxis title = "Hour" tickFormat={v => (new Date(v)).toString()} tickLabelAngle={-45} />
+          <DiscreteColorLegend items={labels} orientation={"horizontal"}/>
           <LineMarkSeries 
             curve={'curveMonotoneX'}
-            color={'#9c88ff'}
+            color={'#FF6978'}
             style= {{fill: 'none'}}
-            data={t454_hum}
+            data={lab_hum}
             opacity = {1}
+             
           />
           <LineMarkSeries 
             curve={'curveMonotoneX'}
-            color={'#273c75'}
+            color={'#352D39'}
             style= {{fill: 'none'}}
-            data={t454_temp}
+            data={lab_temp}
             opacity = {1}
+          />
+        </XYPlot>
+        <XYPlot width={1000} height={800} margin={{left: 100, right: 10, top: 10, bottom:300}}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <YAxis title = "Temperature/Humidity (°F)"/>
+          <XAxis title = "Hour" tickFormat={v => (new Date(v)).toString()} tickLabelAngle={-45} />
+          <DiscreteColorLegend items={labels} orientation={"horizontal"}/>
+          <LineMarkSeries 
+            curve={'curveMonotoneX'}
+            color={'#FF6978'}
+            style= {{fill: 'none'}}
+            data={lab_hum2}
+            opacity = {1}
+             
           />
           <LineMarkSeries 
             curve={'curveMonotoneX'}
-            color={'#b71540'}
+            color={'#352D39'}
             style= {{fill: 'none'}}
-            data={h309_hum}
+            data={lab_temp2}
             opacity = {1}
           />
-          <LineMarkSeries 
-            curve={'curveMonotoneX'}
-            color={'#e55039'}
-            style= {{fill: 'none'}}
-            data={h309_temp}
-            opacity = {1}
-          />
-          <Highlight
-            drag
-            enableX={false}
-            /* onDrag={area => (this as any).setState({filter: area})}*//>
-
         </XYPlot>
       </Container>
     </>
